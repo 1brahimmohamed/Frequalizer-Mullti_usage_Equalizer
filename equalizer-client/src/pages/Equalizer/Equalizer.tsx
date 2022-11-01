e4e028b3f669b1ed04cd2b384f922c802198a55f
Kamel Mohamed <112772763+Kamel-Mohammed@users.noreply.github.com>
2022-11-01 11:52:11 +0200

    feat: adding equalizer-client structure

---
+++ equalizer-client/src/pages/Equalizer/Equalizer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Equalizer.css'
+
+const Equalizer = () => {
+    return(
+        <h1>
+            Equalizer
+        </h1>
+    )
+}
+
+export default Equalizer;
