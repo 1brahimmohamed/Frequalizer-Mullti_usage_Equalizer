046cbc17b2e988180c305535808401db071e391f
Ibrahim <imimazim7@gmail.com>
2022-11-03 19:08:38 +0200

    added slider component

--- equalizer-client/src/App.tsx
+++ equalizer-client/src/App.tsx
@@ -1,14 +1,19 @@
 import Equalizer from './pages/Equalizer/Equalizer'
 import './App.css';
 import Header from './componenets/Header/Header';
+import Slider from "./componenets/Slider/slider";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Equalizer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Equalizer/>
+            <Slider/>
+            <Slider/>
+            <Slider/>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ equalizer-client/src/componenets/Slider/slider.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './slider.css'
+
+const Slider = () => {
+    return(
+        <input type={"range"}/>
+    )
+}
+export default Slider;
