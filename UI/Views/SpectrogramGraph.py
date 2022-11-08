b4d0c74fb33031cde18d029fa621f41e63e32a7d
Kamel Mohamed <112772763+Kamel-Mohammed@users.noreply.github.com>
2022-11-09 01:49:43 +0200

    feat: updating upload button/specgram

--- UI/Views/SpectrogramGraph.py
+++ UI/Views/SpectrogramGraph.py
@@ -1,31 +1,33 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-import plotly.graph_objects as go
+import numpy as np
+from state_management.state_management import state_management
+import librosa 
+import librosa.display as dsp
+
 
 
 class SpectrogramGraph():
     def __init__(self):
-        self.fig = go.Figure()
-        self.fig.update_layout(yaxis_title="Spectogram")
-        self.fig.update_xaxes(showgrid=False, automargin=True)
-        self.fig.update_yaxes(showgrid=False, automargin=True)
+        State = state_management()
 
-        self.fig.update_layout(
-            height=180,
-            margin={
-                'l': 0,
-                'r': 0,
-                'b': 0,
-                't': 0
-            }
-        )
-        self.fig.update_layout(legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=1.02,
-            xanchor="right",
-            x=1
-        ))
-        
-        st.plotly_chart(self.fig)
+        fig, ax = plt.subplots(figsize=(8, 4))
+        if len(st.session_state.currentSignal['signal']) != 0:
+            signal = np.array(st.session_state.currentSignal['signal'])
+            sampleRate = st.session_state.currentSignal['sampleRate']
+            y_percussive = librosa.effects.percussive(signal, margin=5)
+            d = librosa.stft(y_percussive)
+            D = librosa.amplitude_to_db(np.abs(d),ref=np.max)
+
+            fig,ax = plt.subplots(2,1,sharex=True,figsize=(10,10))
+
+            img = dsp.specshow(D, y_axis='linear', x_axis='s',sr=sampleRate,ax=ax[0])
+
+            ax[0].set(title='Linear frequency power spectrogram')
+            ax[0].label_outer()
+            dsp.specshow(D,y_axis='log',x_axis='s',sr=sampleRate,ax=ax[1])
+            ax[1].set(title='Log frequency power spectrogram')
+            ax[1].label_outer()
+            fig.colorbar(img, ax=ax, format='%+2.f dB')
+
+        st.pyplot(fig)
